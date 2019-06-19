a82cf3b2162ea31fb0d587831f9ccf251884bcdf
adamf124 <adam.ferguson124@gmail.com>
2019-06-19 15:04:35 -0400

    number types in ts

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
 console.log('Hello TypeScript!');
+const pizzaCost: number = 10;
+const pizzaToppings: number = 2;
+
+function calcCost(cost: number, toppings:number):number {
+  return cost + 1.5 * toppings
+}
+
+const cost: number = calcCost(pizzaCost, pizzaToppings);
+console.log(`The pizza costs: ${cost}`);
