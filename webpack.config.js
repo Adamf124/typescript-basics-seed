4529b5288a8cb9251216d3b53a0ee2306e745706
adamf124 <adam.ferguson124@gmail.com>
2019-06-19 13:57:29 -0400

    update config for ts-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,18 @@
 module.exports = {
+  mode: "development",
+  devtool: "inline-source-map",
   entry: './src/app.ts',
   output: {
-    filename: 'app.js',
+    filename: "bundle.js",
     path: __dirname + './dist'
   },
   resolve: {
-    extensions: ['.ts', '.js']
+    extensions: ['.ts', '.tsx', '.js']
   },
   module: {
     rules: [{
       test: /\.ts$/,
-      use: 'awesome-typescript-loader'
+      loader: "ts-loader"
     }]
   },
   devServer: {
