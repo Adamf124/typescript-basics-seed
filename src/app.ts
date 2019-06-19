7430365cc5e490b6cf7daabf633286fd2c5b65a6
adamf124 <adam.ferguson124@gmail.com>
2019-06-19 15:13:29 -0400

    END STRING PRACTICE

--- src/app.ts
+++ src/app.ts
@@ -8,3 +8,14 @@ function calcCost(cost: number, toppings:number):number {
 
 const cost: number = calcCost(pizzaCost, pizzaToppings);
 console.log(`The pizza costs: ${cost}`);
+// <----------- END Number Type Practice ----------->
+
+const coupon: string = 'pizza21';
+
+function normalizeCoupon(code: string): string {
+  return code.toUpperCase();
+}
+
+const couponMessage: string = `Final coupon is ${normalizeCoupon(coupon)}`;
+console.log(couponMessage);
+// <----------- END STRING PRACTICE ------------>
