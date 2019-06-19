54b3ec8690fe57fdb8737b7b899cf5d2d99c1394
adamf124 <adam.ferguson124@gmail.com>
2019-06-19 15:42:20 -0400

    Boolean practice

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,38 @@
-console.log('Hello TypeScript!');
-const pizzaCost: number = 10;
-const pizzaToppings: number = 2;
+// console.log('Hello TypeScript!');
+// const pizzaCost: number = 10;
+// const pizzaToppings: number = 2;
 
-function calcCost(cost: number, toppings:number):number {
-  return cost + 1.5 * toppings
-}
+// function calcCost(cost: number, toppings:number):number {
+//   return cost + 1.5 * toppings
+// }
 
-const cost: number = calcCost(pizzaCost, pizzaToppings);
-console.log(`The pizza costs: ${cost}`);
-// <----------- END Number Type Practice ----------->
+// const cost: number = calcCost(pizzaCost, pizzaToppings);
+// console.log(`The pizza costs: ${cost}`);
+// // <----------- END Number Type Practice ----------->
 
-const coupon: string = 'pizza21';
+// const coupon: string = 'pizza21';
 
-function normalizeCoupon(code: string): string {
-  return code.toUpperCase();
-}
+// function normalizeCoupon(code: string): string {
+//   return code.toUpperCase();
+// }
 
-const couponMessage: string = `Final coupon is ${normalizeCoupon(coupon)}`;
-console.log(couponMessage);
-// <----------- END STRING PRACTICE ------------>
+// const couponMessage: string = `Final coupon is ${normalizeCoupon(coupon)}`;
+// console.log(couponMessage);
+// // <----------- END STRING PRACTICE ------------>
+
+// const pizzas: number = 5
+
+// function offerDiscount(orders: number): boolean {
+//   return orders >= 3;
+
+// }
+
+// if (offerDiscount(pizzas)){
+//   console.log(`Discount on your ${pizzas} pizzas`);
+// }
+// else{
+//   console.log(`Order more pizzas`);
+// }
+
+// <---------- END BOOL PRACTICE ------------>
+
